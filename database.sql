cae17baea6824cb5775148fe6044ba2bef95c05a
Anton Toporkov <antontoporkoff@gmail.com>
2022-05-24 23:55:52 +0300

    lab5 complete

---
+++ database.sql
@@ -0,0 +1,55 @@
+--
+-- Скрипт создания базы данных с данными.
+--
+
+-- Удалить старую базу и аккаунт, если они существуют
+DROP DATABASE IF EXISTS lab5;
+DROP USER IF EXISTS lab5User;
+
+-- Создать новую базу и аккаунт
+CREATE DATABASE lab5;
+CREATE USER lab5User IDENTIFIED BY 'lab5Password';
+GRANT ALL PRIVILEGES ON lab5.* TO lab5User;
+
+-- Использовать созданную базу
+USE lab5;
+
+-- Создать таблицу без партицирования
+CREATE TABLE IF NOT EXISTS `transactions_no_part` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(32) NOT NULL,
+    `posts_count` DOUBLE NOT NULL DEFAULT 0.0,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB;
+
+-- Создать таблицу с партицированием
+CREATE TABLE IF NOT EXISTS `transactions_part` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(32) NOT NULL,
+    `posts_count` DOUBLE NOT NULL DEFAULT 0.0,
+    PRIMARY KEY (`id`)
+) ENGINE = InnoDB
+  PARTITION BY KEY(`id`)
+  PARTITIONS 10;
+
+
+-- Создать процедуру для генерации случайных данных
+DELIMITER //
+CREATE PROCEDURE `generate_data` (IN items_count INT)
+BEGIN
+    DECLARE i INT DEFAULT 0;
+    WHILE i < items_count DO
+        INSERT INTO `transactions_no_part` (`title`, `posts_count`)  VALUES (MD5(RAND()),RAND() * 1000);
+        SET i = i + 1;
+    END WHILE;
+    -- Скопировать те же данные в партицированную таблицу
+    INSERT INTO `transactions_part`
+        SELECT * FROM `transactions_no_part`;
+END//
+DELIMITER ;
+
+-- Заполнить таблицу данными
+CALL generate_data(100000);
+
+-- Удалить процедуру
+DROP PROCEDURE `generate_data`;
--- server.js
+++ server.js
@@ -1,26 +1,77 @@
+const express = require('express');
+const mysql = require('mysql2/promise');
+const hirestime = require('hirestime')
 
 'use strict';
 
-const express = require('express');
-
+// Параметры web-сервера
 const server_port = '3000';
-const server_host = '127.0.0.1';
+const server_host = 'localhost';
 
-const app = express();
+// Параметры подключения к MySQL
+const conn = { host: 'localhost', port: 3305, database: 'lab5', user: 'lab5User', password : 'lab5Password' };
 
-app.set('view engine', 'pug');
-app.get('/', (req, res, next) => {
-    let data = [
-        [ 1, 'Item 1' ],
-        [ 2, 'Item 2' ],
-        [ 3, 'Item 3' ],
-    ];
-    res.render('index', {
-        header: 'Example Express web server using Pug template engine',
-        text: 'The table below is shown using data from JavaScript array:',
-        table: data,
-    })
-});
-app.listen(server_port, server_host, () => {
-    console.log(`Server running at http://${server_host}:${server_port}/, press Ctrl-C to exit`);
-});
+// Номер id для оценки времени
+const test_id = 100;
+const row_count = 100;
+
+// Тестовые запросы
+const queries = [
+    { table: 'transactions_no_part', sql: `SELECT SQL_NO_CACHE * FROM transactions_no_part LIMIT ${row_count}` },
+    // { table: 'transactions_no_part', sql: `SELECT SQL_NO_CACHE * FROM transactions_no_part ORDER BY id DESC LIMIT ${row_count}` },
+    { table: 'transactions_no_part', sql: `SELECT SQL_NO_CACHE * FROM transactions_no_part WHERE id = ${test_id} LIMIT 1` },
+    { table: 'transactions_part',    sql: `SELECT SQL_NO_CACHE * FROM transactions_part LIMIT ${row_count}` },
+    // { table: 'transactions_part',    sql: `SELECT SQL_NO_CACHE * FROM transactions_part ORDER BY id DESC LIMIT ${row_count}` },
+    { table: 'transactions_part',    sql: `SELECT SQL_NO_CACHE * FROM transactions_part WHERE id = ${test_id} LIMIT 1` },
+];
+
+async function run() {
+    // Установить соединение с сервером
+    const db = await mysql.createConnection(conn);
+
+    // Создать web сервис
+    const app = express();
+
+    // Использовать pug (бывший jade) как HTML template движок
+    app.set('view engine', 'pug');
+
+    // По запросу http://server:port/ выполнить 4 тестовых выборки из двух таблиц
+    app.get('/', wrapAsync(async (req, res, next) => {
+        // Получить количество элементов в таблице (только для отчета)
+        const [ rows ] = await db.execute(`SELECT COUNT(*) AS n FROM ${queries[0].table}`);
+        const n = rows[0].n;
+
+        // Выполнить тестовые выборки, накапливая результаты в массиве results
+        let results = [];
+        for (let i in queries) {
+            let time = hirestime.hirestimeNode();
+            let [ rows ] = await db.execute(queries[i].sql);
+            let elapsed = time.ms().toFixed(2);
+
+            results.push({
+                n: n,
+                sql: queries[i].sql,
+                table: queries[i].table,
+                time: elapsed,
+            });
+        }
+
+        // Отобразить страницу результатов с помощью pug по шаблону results.pug
+        res.render('results', { header: `Query results`, results: results });
+    }));
+
+    // Запустить HTTP сервер
+    app.listen(server_port, server_host, () => {
+        console.log(`Server running at http://${server_host}:${server_port}/, press Ctrl-C to exit`);
+    });
+}
+
+// Вспомогательная функция для обработки возможных ошибок в асинхронных функциях.
+// Переназначает exceptions на стандартный обработчик ошибок Express.
+function wrapAsync(fn) {
+    return function(req, res, next) {
+        fn(req, res, next).catch(next);
+    };
+}
+
+run();
