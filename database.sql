2dd95d2d218cf9106780064d035b350d00f98a13
Anton Toporkov <antontoporkoff@gmail.com>
2022-05-25 03:22:36 +0300

    lab 7 finished

--- database.sql
+++ database.sql
@@ -1,55 +1,37 @@
 --
--- Скрипт создания базы данных с данными.
+-- Скрипт создания базы данных
 --
 
 -- Удалить старую базу и аккаунт, если они существуют
-DROP DATABASE IF EXISTS lab5;
-DROP USER IF EXISTS lab5User;
+DROP DATABASE IF EXISTS lab7;
+DROP USER IF EXISTS lab7User;
 
 -- Создать новую базу и аккаунт
-CREATE DATABASE lab5;
-CREATE USER lab5User IDENTIFIED BY 'lab5Password';
-GRANT ALL PRIVILEGES ON lab5.* TO lab5User;
+CREATE DATABASE lab7;
+CREATE USER lab7User IDENTIFIED BY 'lab7Password';
+GRANT ALL PRIVILEGES ON lab7.* TO lab7User;
 
 -- Использовать созданную базу
-USE lab5;
+USE lab7;
 
--- Создать таблицу без партицирования
-CREATE TABLE IF NOT EXISTS `blogs_no_part` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
+
+-- Создать таблицу
+CREATE TABLE IF NOT EXISTS `blogs` (
+    `id` INTEGER NOT NULL,
     `title` VARCHAR(32) NOT NULL,
     `posts_count` DOUBLE NOT NULL DEFAULT 0.0,
     PRIMARY KEY (`id`)
 ) ENGINE = InnoDB;
 
--- Создать таблицу с партицированием
-CREATE TABLE IF NOT EXISTS `blogs_part` (
-    `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `title` VARCHAR(32) NOT NULL,
-    `posts_count` DOUBLE NOT NULL DEFAULT 0.0,
-    PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  PARTITION BY KEY(`id`)
-  PARTITIONS 10;
-
 
 -- Создать процедуру для генерации случайных данных
 DELIMITER //
-CREATE PROCEDURE `generate_data` (IN items_count INT)
+CREATE PROCEDURE `generate_data` (IN start_id INT, IN items_count INT)
 BEGIN
-    DECLARE i INT DEFAULT 0;
-    WHILE i < items_count DO
-        INSERT INTO `blogs_no_part` (`title`, `posts_count`)  VALUES (MD5(RAND()),RAND() * 1000);
+    DECLARE i INT DEFAULT start_id;
+    WHILE i < start_id + items_count DO
+        INSERT INTO `blogs` (`id`,`title`, `posts_count`)  VALUES (i,MD5(RAND()),RAND() * 1000);
         SET i = i + 1;
     END WHILE;
-    -- Скопировать те же данные в партицированную таблицу
-    INSERT INTO `blogs_part`
-        SELECT * FROM `blogs_no_part`;
 END//
 DELIMITER ;
-
--- Заполнить таблицу данными
-CALL generate_data(100000);
-
--- Удалить процедуру
-DROP PROCEDURE `generate_data`;
--- server.js
+++ server.js
@@ -1,33 +1,33 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
-const hirestime = require('hirestime')
 
 'use strict';
 
 // Параметры web-сервера
-const server_port = '3000';
 const server_host = 'localhost';
+const server_port = '3000';
+
+// Количество серверов и максимальное количество записей на сервере согласно варианту
+n_servers = 4
+n_records = 5;
+
+// Имена серверов для отображения
+const server_names = [ 'server1', 'server2', 'server3', 'server4' ];
 
 // Параметры подключения к MySQL
-const conn = { host: 'localhost', port: 3305, database: 'lab5', user: 'lab5User', password : 'lab5Password' };
-
-// Номер id для оценки времени
-const test_id = 100;
-const row_count = 100;
-
-// Тестовые запросы
-const queries = [
-    { table: 'blogs_no_part', sql: `SELECT SQL_NO_CACHE * FROM blogs_no_part LIMIT ${row_count}` },
-    // { table: 'blogs_no_part', sql: `SELECT SQL_NO_CACHE * FROM blogs_no_part ORDER BY id DESC LIMIT ${row_count}` },
-    { table: 'blogs_no_part', sql: `SELECT SQL_NO_CACHE * FROM blogs_no_part WHERE id = ${test_id} LIMIT 1` },
-    { table: 'blogs_part',    sql: `SELECT SQL_NO_CACHE * FROM blogs_part LIMIT ${row_count}` },
-    // { table: 'blogs_part',    sql: `SELECT SQL_NO_CACHE * FROM blogs_part ORDER BY id DESC LIMIT ${row_count}` },
-    { table: 'blogs_part',    sql: `SELECT SQL_NO_CACHE * FROM blogs_part WHERE id = ${test_id} LIMIT 1` },
+const conn = [
+    { host: 'localhost', port: 3301, database: 'lab7', user: 'lab7User', password : 'lab7Password' },
+    { host: 'localhost', port: 3302, database: 'lab7', user: 'lab7User', password : 'lab7Password' },
+    { host: 'localhost', port: 3303, database: 'lab7', user: 'lab7User', password : 'lab7Password' },
+    { host: 'localhost', port: 3304, database: 'lab7', user: 'lab7User', password : 'lab7Password' },
 ];
 
 async function run() {
-    // Установить соединение с сервером
-    const db = await mysql.createConnection(conn);
+    // Установить соединение с серверами
+    let db = [];
+    for (let i = 0; i < conn.length; i++) {
+        db[i] = await mysql.createConnection(conn[i]);
+    }
 
     // Создать web сервис
     const app = express();
@@ -35,29 +35,57 @@ async function run() {
     // Использовать pug (бывший jade) как HTML template движок
     app.set('view engine', 'pug');
 
-    // По запросу http://server:port/ выполнить 4 тестовых выборки из двух таблиц
+    // По запросу http://server:port/ получить все записи из базы данных с серверов
     app.get('/', wrapAsync(async (req, res, next) => {
-        // Получить количество элементов в таблице (только для отчета)
-        const [ rows ] = await db.execute(`SELECT COUNT(*) AS n FROM ${queries[0].table}`);
-        const n = rows[0].n;
-
-        // Выполнить тестовые выборки, накапливая результаты в массиве results
-        let results = [];
-        for (let i in queries) {
-            let time = hirestime.hirestimeNode();
-            let [ rows ] = await db.execute(queries[i].sql);
-            let elapsed = time.ms().toFixed(2);
-
-            results.push({
-                n: n,
-                sql: queries[i].sql,
-                table: queries[i].table,
-                time: elapsed,
-            });
+        // Показать все записи
+        let rows = [];
+        for (let i = 0; i < db.length; i++) {
+            [ rows[i] ] = await db[i].execute('SELECT * FROM blogs ORDER BY id');
         }
 
-        // Отобразить страницу результатов с помощью pug по шаблону results.pug
-        res.render('results', { header: `Query results`, results: results });
+        // Отобразить страницу с помощью pug по шаблону index.pug
+        res.render('index', {
+            header: `Database content from all servers`,
+            names: server_names,
+            conn: conn,
+            rows: rows,
+        });
+    }));
+
+    // По запросу http://server:port/id/<id> получить нужную запись с соответствующего сервера
+    app.get('/id/:id', wrapAsync(async (req, res, next) => {
+        // Получить id из запроса
+        let id = req.params.id;
+        let max_id = n_servers * n_records - 1;
+
+        // Вернуть ошибку, если вне допустимого диапазона
+        if ((id < 0) || (id > max_id)) {
+            res.render('record', {
+                header: `Record ${id} is out of range`,
+                msg: `Valid range is ${0}..${max_id}`,
+                rows: [],
+            });
+            return;
+        }
+
+        // Найти номер сервера по id записи
+        let server_id = Math.floor(id / n_records);
+
+        // Использовать соответствующее подключение для выборки записи
+        const [ rows ] = await db[server_id].execute('SELECT * FROM blogs WHERE id = ?', [ id ]);
+
+        // Отобразить страницу с помощью pug по шаблону record.pug
+        if (rows.length) {
+            res.render('record', {
+                header: `Record ${id} was found on ${server_names[server_id]}`,
+                rows: rows,
+            });
+        } else {
+            res.render('record', {
+                header: `Record ${id} is expected to be on ${server_names[server_id]}, but was not found`,
+                rows: rows,
+            });
+        }
     }));
 
     // Запустить HTTP сервер
